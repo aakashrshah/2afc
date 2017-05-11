504d85499a52782d5c7d6a0da3ad36991766a58f
aakashrshah <aadi1428@gmail.com>
2017-05-11 01:29:25 -0400

    Component Structure defined

--- src/App.js
+++ src/App.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+let Survey = require('survey-react');
+
+Survey.defaultBootstrapCss.navigationButton = "btn btn-primary";
+Survey.Survey.cssType = "bootstrap";
+var survey = new Survey.Model( { questions: [
+     {name:"name", type:"text", title: "Please enter your name:", placeHolder:"Jon Snow", isRequired: true},
+     {name:"birthdate", type:"text", inputType:"date", title: "Your birthdate:", isRequired: true},
+     {name:"color", type:"text", inputType:"color", title: "Your favorite color:"},
+     {name:"email", type:"text", inputType:"email", title: "Your e-mail:", placeHolder:"jon.snow@nightwatch.org", isRequired: true, validators: [{type:"email"}]}
+]});
 
 class App extends Component {
   render() {
@@ -13,6 +23,7 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <Survey.Survey model={survey} />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,66 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from '/App';
+import App from './App';
 import './index.css';
-
-import $ from "jquery";
 
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
-
-
-//ReactDOM.render(<Survey.Survey model={survey} />, document.getElementById("surveyElement"));
-
-
-/*
-Survey.JsonObject.metaData.addProperty("dropdown", {name: "renderAs", default: "standard", choices: ["standard", "imagepicker"]});
-var survey = new Survey.Model({ questions: [
- { type: "dropdown", name: "choosepicture", renderAs: "imagepicker", title: "What animal would you like to see first ?", 
-     choices: [
-        {value: "lion", text: "../../images/image-picker/lion.jpg"},
-        {value: "giraffe", text: "../../images/image-picker/giraffe.jpg"},
-        {value: "panda", text: "../../images/image-picker/panda.jpg"},
-        {value: "camel", text: "../../images/image-picker/camel.jpg"}
-     ]
-  }
-]});
-
-
-var widget = {
-    name: "imagepicker",
-    isFit : function(question) { return question["renderAs"] === 'imagepicker'; },
-    isDefaultRender: true,
-    afterRender: function(question, el) {
-
-        var $el = $(el).find("select");
-
-        var options = $el.find('option');
-        for (var i=1; i<options.length; i++) {
-            options[i].dataset["imgSrc"] = options[i].text;
-        }
-        $el.imagepicker({
-            hide_select : true,
-            show_label  : false,
-            selected: function(opts) {
-                question.value = opts.picker.select[0].value;
-            }
-        })
-    }
-    
-        ,
-        willUnmount: function(question, el) {
-            var $el = $(el).find("select");
-            $el.data('picker').destroy();
-        } 
-    
-}
-
-Survey.CustomWidgetCollection.Instance.addCustomWidget(widget);
-
-
-//ReactDOM.render(<Survey.Survey model={survey}/>, document.getElementById("surveyElement"));
-
-*/
